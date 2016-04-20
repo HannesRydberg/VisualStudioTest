695e5f9be4bfd2ea8cb424c41d08241678af1315
Hannes Rydberg <mrhrxx@gmail.com>
2016-04-20 22:15:20 +0200

    Added some basic testing of inputs, lists and Console.Write and what not. Now writes some shit then reads and repeat persons. Communicates with a "Person"-class in a separate file.

---
+++ GitTest/GitTest/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace GitTest
+{
+
+    public class Person
+    {
+        private String firstName; 
+        private String lastName;
+        private String pNbr;
+
+
+
+        public Person()
+        {
+         
+
+        }
+
+        public Person(String firstName, String lastName, String pNbr)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.pNbr = pNbr;
+        }
+ 
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string PNbr
+        {
+            get
+            {
+                return pNbr;
+            }
+
+            set
+            {
+                pNbr = value;
+            }
+        }
+    }
+}
--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -10,10 +10,55 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person("Jan", "Jansson", "880422-7744");
             Console.WriteLine("Let's git it on!");
-            Console.Read();
+            Console.ReadLine();
             Console.WriteLine("Gittin' it on!");
+            Console.ReadLine();
+            Console.WriteLine("{0} gits it!", p1.FirstName);
+            Console.Write("Write something: ");
+            String repeat = Console.ReadLine();
+            Console.WriteLine(repeat);
+            Console.ReadLine();
+
+            List<Person> pList = new List<Person>();
+            bool run = true;
+            while (run)
+            {
+                Person p = new Person();
+                Console.Write("Please enter the persons first name: ");
+                p.FirstName = Console.ReadLine();
+                Console.Write("Please enter the persons last name: ");
+                p.LastName = Console.ReadLine();
+                Console.Write("Please enter the persons personal number: ");
+                p.PNbr = Console.ReadLine();
+                pList.Add(p);
+                Console.Write("{0} {1} added to persons. Add another person (Y/N)? ", p.FirstName, p.LastName);
+                ConsoleKeyInfo input = Console.ReadKey();
+
+                while (input.KeyChar != 'y' && input.KeyChar != 'n')
+                {
+                    Console.Write("\n Not a valid selection. Add another person (Y/N)?");
+                    input = Console.ReadKey();
+                }
+
+                if (input.KeyChar == 'n' )
+                {
+                    run = false;
+                }
+                Console.WriteLine();
+              
+            }
+
+            Console.WriteLine("The following persons have been added: ");
+            foreach(Person p in pList){
+                Console.WriteLine("{0} {1} {2}", p.FirstName, p.LastName, p.PNbr);
+
+            }
+            Console.WriteLine("That was everyone! Press any key to exit.");
             Console.Read();
+
+
         }
     }
 }
